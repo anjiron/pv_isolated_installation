d26ca3ade8ba9adce377dcd5fb91d4f58385312f
t731921 <t731921>
2021-07-25 00:47:03 +0200

    rendimiento instalacion

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ class PvInsolated():
         self.dispositivos()
         self.add_calculo_energia()
         self.potencia_total()
-        self.energia_total()
+        self.rendimiento_instalacion()
         return
     
+    # Estos datos de entra en el futuro vendrán desde web 
     def dispositivos(self):
         self.datos = {
                     'luminarias': {'numero': 16, 'potencia': 10, 'horas': 6},
-                    'ordenadores': {'numero': 2, 'potencia': 70, 'horas': 2},
+                    'ordenadores': {'numero': 2, 'potencia': 100, 'horas': 2},
                     'frigorifico': {'numero': 3, 'potencia': 700, 'horas': 12},
+                    'ventilador': {'numero': 2, 'potencia': 80, 'horas': 4},
+                    'lavadora': {'numero': 1, 'potencia': 800, 'horas': 1},
                     'television': {'numero': 2, 'potencia': 100, 'horas': 4}
                     }
         return 
@@ -26,14 +29,19 @@ class PvInsolated():
         p_total = 0
         for i in self.datos.values():
             p_total = p_total + i['potencia']   
-        return print(p_total)
+        return p_total
               
 
     def energia_total(self):
         e_total = 0
         for i in self.datos.values():
             e_total = e_total + i['energia']   
-        return print(e_total)
+        return e_total
+    
+    def rendimiento_instalacion(self):
+        n = 0.8
+        energia = self.energia_total() / n
+        return print(energia)
 
 
 
