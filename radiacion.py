49beb1c3285a7e63c13a5d60b9f8f8168f0f8be9
anjirom <anjirom@gmail.com>
2021-07-28 18:57:00 +0200

    busqueda radiacion

--- radiacion.py
+++ radiacion.py
@@ -1,11 +1,24 @@
 
-from selenium import webdriver
+import pandas
 
-fp = webdriver.FirefoxProfile()
-driver = webdriver.Firefox(fp)
-url = 'http://www.adrase.ciemat.es/mapa-zona-peninsula/index.php'
-driver.get(url)
+class GetRadiacion():
+    def __init__(self):
+        self.latitud = 38.27
+        self.longitud = -3.05
 
-driver.find_element_by_name("lat_question").send_keys('30')
-driver.find_element_by_name("lon_question").send_keys('-3')
+    def obtener_radiacion_min(self):
 
+        url = 'http://www.adrase.com/adrasemaps/php/monthly_popup.php?lat=' + str(self.latitud) + '&lon=' + str(self.longitud) + '&var_tipe=0'
+        
+        try:
+            df_tabla_resultados = pandas.read_html(url)  
+        except: print("EL VALOR DE LATITUD O LONGITUD INTRODUCIDO ESTÁ FUERA DE RANGO\n VALORES VÁLIDOS: ESPAÑA\n")
+
+        print(df_tabla_resultados[0])
+        valores_minimos = df_tabla_resultados[0].min(numeric_only=True, axis=1)
+        rad_media_min = valores_minimos.iloc[2]
+        
+        return rad_media_min
+
+
+# print(GetRadiacion().obtener_radiacion_min())
