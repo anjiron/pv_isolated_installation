9bfe22b2e548bedc6d813b39e77a91486ca2c653
anjirom <anjirom@gmail.com>
2021-10-23 20:14:44 +0200

    views y urls creadas en django

--- pv_isolated/pv_isolated_app/views.py
+++ pv_isolated/pv_isolated_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+
+    return render(request, "pv_isolated_app/home.html")
+
+
+def calculadora(request):
+
+    return render(request, "pv_isolated_app/calculadora.html")
