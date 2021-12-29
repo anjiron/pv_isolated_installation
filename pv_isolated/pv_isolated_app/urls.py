6d439c5dc62dcf9fad3b117fc3ef6d9eeb6ed23a
anjiron <anjirom@gmail.com>
2021-12-29 17:19:35 +0100

    pruebas mostrar variables

--- pv_isolated/pv_isolated_app/urls.py
+++ pv_isolated/pv_isolated_app/urls.py
@@ -7,4 +7,5 @@ from pv_isolated_app import views
 urlpatterns = [
     path('', views.home, name="HOME"),
     path('input_data/', views.input_data, name="INPUT_DATA"),
+    path('show_data/', views.show_data, name="SHOW_DATA"),
 ]
--- pv_isolated/pv_isolated_app/views.py
+++ pv_isolated/pv_isolated_app/views.py
@@ -10,3 +10,10 @@ def home(request):
 def input_data(request):
 
     return render(request, "pv_isolated_app/input_data.html")
+
+def show_data(request):
+    #luminarias_req = request.GET['luminarias']
+    luminarias_req = {}
+    luminarias_req['some_string'] = "sdjfñlaksjdfñalsdkjfañslkdfj"
+
+    return render(request, "pv_isolated_app/show_data.html", luminarias_req)
